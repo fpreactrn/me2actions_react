0ff9243d4f35ae967210999c11a569bbeec3d3ae
fpreactrn <fpreactrn@gmail.com>
2021-06-03 02:19:35 -0400

    deploy useUnifiedTopology: true

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4011;
+const port = process.env.PORT || 4012;
 const mongoose = require('mongoose');
-const User = require('./models/User');
 require('dotenv').config();
+const User = require('./models/User');
 
-const dbUrl = process.env.dbUrl;
+const dbUrl = process.env.dbUrl
 
-mongoose.connect(dbUrl, { 
+mongoose.connect(dbUrl, {
   useUnifiedTopology: true, 
-  useNewUrlParser: true 
+  useNewUrlParser: true
 }, function(err, res){
   if (err) {
     console.log('database connection failed: '+err)
@@ -20,17 +20,17 @@ mongoose.connect(dbUrl, {
 
 app.use(express.json());
 
-// app.use('/', (req, res) => {
+// app.get('/', (req, res) => {
 //   res.send('hello world');
 // });
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username }).exec();  
+  const user = await User.findOne({ username }).exec();
 
   if (user) {
     res.json({
-      message: 'user already exsits'
+      message: 'user already exists'
     });
     return;
   }
@@ -43,29 +43,29 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username }).exec();  
-
-  if (!user || user.password !== password) {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).exec();
+  
+    if (!user || user.password !== password)  {
+      res.json({
+        message: 'invalid login'
+      });
+      return;
+    };
+  
     res.json({
-      message: 'invalid login'
+      message: 'logged in'
     });
-    return;
-  };
-
-  res.json({
-    message: 'logged in'
-  })
-});
+  });
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 };
 
 
 app.listen(port, () => {
-  console.log(`Server listening at port: ${port}`);
+  console.log(`Server listening at: ${port}`);
 });
